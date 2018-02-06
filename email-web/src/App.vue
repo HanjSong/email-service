8de731b8591cddd38df22ac583fcfb047882a7c7
hsong <songgkswn@gmail.com>
2018-02-06 23:22:39 +1100

    mv msg to vue mounted event

--- email-web/src/App.vue
+++ email-web/src/App.vue
@@ -3,7 +3,8 @@
         <div class="logo mb-2">
             <img class="logo-img" src="./assets/logo.jpg">
         </div>
-        <div id="hidden-ie-msg"></div>
+        <div id="hidden-ie-msg">
+        </div>
         <router-view/>
     </div>
 </template>
@@ -11,18 +12,21 @@
 <script>
 export default {
     name: 'App',
-    created: function () {
-        if (navigator.appName.indexOf("Internet Explorer") != -1) { // IE Check
+    mounted: function () {
+        if (navigator.appName.indexOf('Internet Explorer') > -1) { // IE Check
             let noticeMsg = 'Internet Explorer 9 or below is not supported'
-            if (navigator.appVersion.indexOf("MSIE 1") > -1) { // Version 10 or 11
+            if (navigator.appVersion.indexOf('MSIE 1') > -1) { // Version 10 or 11
                 noticeMsg = 'For better experience, please use modern browsers instead of IE'
             }
-
-            let ieNoticeHtml = `<div class="row justify-content-center"><div class="mt-3 col-8">` +
-            `<div role="alert" aria-live="polite" aria-atomic="true" class="alert alert-danger" id="message">` +
-            noticeMsg + `</div></div></div>`
-
-            document.getElementById('hidden-ie-msg').innerHTML = ieNoticeHtml
+            let outDiv = document.createElement('div')
+            outDiv.classList.add('row')
+            outDiv.classList.add('justify-content-center')
+            let innerDiv = document.createElement('div')
+            innerDiv.classList.add('alert-warning')
+            innerDiv.classList.add('alert')
+            innerDiv.textContent = noticeMsg
+            outDiv.appendChild(innerDiv)
+            document.getElementById('hidden-ie-msg').appendChild(outDiv)
         }
     }
 }
